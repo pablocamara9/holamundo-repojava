c04ed76e5884482f089a8eb6dfd3fb38316511bc
Pablo Cámara García <camara.gapab23@triana.salesianos.edu>
2023-11-14 13:55:00 +0100

    Guauuuuuuuu

--- holaMundo.java
+++ holaMundo.java
@@ -1,7 +1,8 @@
 public class holaMundo {
     public static void main(String [] args) {
-    System.out.println("Hola mundo");
-
+        System.out.println("*".repeat(10));
+        System.out.println("Hola mundo");
+        System.out.println("*".repeat(10));
     }
 
 }
